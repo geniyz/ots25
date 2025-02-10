ed45f4010aab151ce1d07529d4e20d42a576b61b
Владимир Овсянников <geniyz@ya.ru>
2025-02-10 16:51:02 +0300

    feat: тест: при первом выбросе исключения повторить команду, при повторном выбросе исключения записать информацию в лог
    восемь пунктов третьей домашки выполнены

--- app/src/commonTest/kotlin/site.geniyz/ExceptionsTest.kt
+++ app/src/commonTest/kotlin/site.geniyz/ExceptionsTest.kt
@@ -79,4 +79,40 @@ class `SOLID и исключения` {
         assertEquals( "site.geniyz.ots.commands.RepeatCommand", commands.removeFirst().toString().substringBefore("@")) // убедиться, что в очереди команд появился повторитель
 
     }
+
+
+    @Test
+    fun `при первом выбросе исключения повторить команду, при повторном выбросе исключения записать информацию в лог`() {
+        val ssBroke = Spaceship(
+            "position" to Vector(0, 0),
+            "velocity" to Vector(Double.NaN, Double.NaN),
+        )
+        val commands = ArrayDeque<Executable>()
+        commands.add(Move(MovableAdpater(ssBroke))) // в очередь команд добавляется «плохая»
+
+        // регистрация повторителя:
+        ExceptionHandler.register(Move::class.toString(), BadVelocity::class.toString()) { c, e -> commands.addFirst( RepeatCommand(c) ) }
+
+        // регистрация записи в лог, при ошибке в повторителе
+        ExceptionHandler.register(RepeatCommand::class.toString(), BadVelocity::class.toString()) { c, e -> commands.addFirst( WriteToLog(c, e) ) }
+
+        commands.doStep() // 1) — первый шаг. Должна произойти ошибка движения
+        // должен стать в очередь повторитель:
+        assertEquals("site.geniyz.ots.commands.RepeatCommand", commands.first().toString().substringBefore("@") ) // убедиться, что в очереди команд появился повторитель
+
+        commands.doStep() // 2) — второй шаг. Должен вызваться повторитель, повторяющий вызов ошибочной команды
+        // в очередь должен встать логгер
+        assertEquals( "site.geniyz.ots.logger.WriteToLog", commands.first().toString().substringBefore("@")) // убедиться, что в очереди команд появился логгер
+
+    }
+}
+
+fun ArrayDeque<Executable>.doStep(): Executable {
+    val command = removeFirst()               // считывается первая (она же плохая, она же единственная) команда
+    try {                                     // Обернуть вызов Команды в блок try-catch.
+        command.execute()
+    }catch (e: Throwable){                    // Обработчик catch должен перехватывать только самое базовое исключение.
+        ExceptionHandler.handle( command, e ) // тут должен произойти вызов повторителя
+    }
+    return command
 }
