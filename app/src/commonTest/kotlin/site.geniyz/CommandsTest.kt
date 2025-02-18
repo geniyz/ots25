77e3d1a4522b62a7a52b76a4d2b5f333af110a65
Владимир Овсянников <geniyz@ya.ru>
2025-02-18 20:16:25 +0300

    класс BurnFuelCommand и тесты к нему

--- app/src/commonTest/kotlin/site.geniyz/CommandsTest.kt
+++ app/src/commonTest/kotlin/site.geniyz/CommandsTest.kt
@@ -1,11 +1,14 @@
 package site.geniyz.ots
 
+import site.geniyz.ots.fuel.BurnFuelCommand
 import site.geniyz.ots.fuel.CheckFuelCommand
 import site.geniyz.ots.fuel.CommandException
 import site.geniyz.ots.fuel.FuelableAdapter
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class `Макрокоманды` {
+
     @Test
     fun `CheckFuelCommand проверяет, что топлива достаточно, если нет, то выбрасывает исключение CommandException → достаточно`() {
         val ss = Spaceship(
@@ -22,4 +25,29 @@ class `Макрокоманды` {
         CheckFuelCommand(FuelableAdapter(ss)).execute()
     }
 
+    @Test
+    fun `Топливо сгорает с 10 до 9 за один шаг`(){
+        val ss = Spaceship(
+            "fuelLevel" to 10L,
+        )
+        BurnFuelCommand(FuelableAdapter(ss)).execute()
+        assertEquals(ss.params["fuelLevel"], 9L)
+    }
+
+    @Test
+    fun `Топливо сгорает с 7 до 4 за один шаг`(){
+        val ss = Spaceship(
+            "fuelLevel" to 7L,
+        )
+        BurnFuelCommand(FuelableAdapter(ss), 3L).execute()
+        assertEquals(ss.params["fuelLevel"], 4L)
+    }
+
+    @Test(expected = CommandException::class)
+    fun `Отрицательный расход топлива`(){
+        val ss = Spaceship(
+            "fuelLevel" to 7L,
+        )
+        BurnFuelCommand(FuelableAdapter(ss), -3L).execute()
+    }
 }
