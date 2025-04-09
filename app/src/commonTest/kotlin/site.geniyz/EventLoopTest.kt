17177539b981fce1b5be3e2dba9be3f4e19ea39d
Владимир Овсянников <geniyz@ya.ru>
2025-04-09 10:57:24 +0300

    test: тест работы в IoC

--- app/src/commonTest/kotlin/site.geniyz/EventLoopTest.kt
+++ app/src/commonTest/kotlin/site.geniyz/EventLoopTest.kt
@@ -2,9 +2,13 @@ package site.geniyz.ots
 
 import org.junit.Test
 import site.geniyz.ots.commands.Executable
+import site.geniyz.ots.el.AddCommands
 import site.geniyz.ots.el.EventLoop
 import site.geniyz.ots.el.HardStopCommand
 import site.geniyz.ots.el.SoftStopCommand
+import site.geniyz.ots.el.StartCommand
+import site.geniyz.ots.ioc.InitCommand
+import site.geniyz.ots.ioc.IoC
 import java.util.concurrent.CountDownLatch
 import kotlin.test.assertEquals
 
@@ -91,5 +95,43 @@ class EventLoopTest {
         assertEquals(6, ss["acc"] as Int)
     }
 
+    @Test
+    fun `В IoC`() {
+        var ss = Spaceship("acc" to 0)
+        var el: EventLoop? = null
+
+        InitCommand().execute()
+
+        IoC.resolve<Executable>("IoC.Register", "el", { _: List<Any?>? ->
+            if(el == null) el = EventLoop()
+            el
+        }).execute()
+        IoC.resolve<Executable>("IoC.Register", "el.add", { args: List<Executable> ->
+            AddCommands(IoC.resolve("el") as EventLoop, args)
+        }).execute()
+        IoC.resolve<Executable>("IoC.Register", "el.start", { _: List<Any?>? ->
+            StartCommand(IoC.resolve("el") as EventLoop)
+        }).execute()
+        IoC.resolve<Executable>("IoC.Register", "el.stop", { _: List<Any?>? ->
+            HardStopCommand(IoC.resolve("el") as EventLoop)
+        }).execute()
+
+        IoC.resolve<Executable>("el.add", AccCommand(ss, 1), AccCommand(ss, 2), AccCommand(ss, 3)).execute()
+        IoC.resolve<Executable>("el.add", IoC.resolve("el.stop")).execute()
+        IoC.resolve<Executable>("el.add", AccCommand(ss, 4), AccCommand(ss, 5), AccCommand(ss, 6)).execute()
+
+        val f = CountDownLatch(1)
+        (IoC.resolve("el") as EventLoop).onEnd = {
+            f.countDown()
+            println(" acc = ${ss["acc"]} ")
+        }
+
+        IoC.resolve<Executable>("el.start").execute()
+
+        f.await()
+        println(" ВСЁ ")
+
+        assertEquals(6, ss["acc"] as Int)
+    }
 
 }
