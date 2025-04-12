5413e9d7ee1482af2b970c6bd9365e87c8ad16f6
Владимир Овсянников <geniyz@ya.ru>
2025-04-12 10:51:55 +0300

    feat: «базовые» объекты для взаимодействия с кафкой

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,6 +12,16 @@ kotlin {
 
                 implementation(kotlin("reflect"))
                 implementation(kotlin("scripting-jsr223"))
+
+                implementation(libs.kafka.client)
+                implementation(libs.kotlinx.atomicfu)
+                // implementation(libs.kotlinx.datetime)
+
+                implementation(libs.kotlinx.serialization.core)
+                implementation(libs.kotlinx.serialization.json)
+
+                implementation(libs.logback.classic)
+
             }
         }
         val commonTest by getting {
